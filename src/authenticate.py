7a0bd8141de8cc58eeee4e4daf3648f9ccf0edfd
ericcheung1 <ericcheun251@gmail.com>
2025-04-15 02:20:30 -0700

    added files

---
+++ src/authenticate.py
@@ -0,0 +1,18 @@
+import configparser
+import praw
+import os
+
+def authenticate_reddit(config_file):
+    project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    config = configparser.ConfigParser()
+    config.read(os.path.join(project_root, config_file))
+    reddit_instance = praw.Reddit(
+        client_id=config.get('reddit', 'client_id'),
+        client_secret=config.get('reddit', 'client_secret'),
+        username=config.get('reddit', 'username'),
+        password=config.get('reddit', 'password'),
+        user_agent="test_bot"
+        )
+    
+    return reddit_instance
+
