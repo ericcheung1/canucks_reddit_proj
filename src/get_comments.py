0133724d04643abf4272298ed682a9320c82bab2
ericcheung1 <ericcheun251@gmail.com>
2025-05-05 22:06:59 -0700

    modified file

--- src/get_comments.py
+++ src/get_comments.py
@@ -11,12 +11,12 @@ subreddit = reddit_instance.subreddit('canucks')
 submission = reddit_instance.submission(id='1g1swzp')
 
 post_data = {
-        "comment_id": submission.id, 
+        "post_id": submission.id, 
+        "title": submission.title,
         "author": submission.author,
         "utc_created": submission.created_utc, 
         "comments": []
         }
-top_level_comments = []
 comment_map = {}
 
 print(submission.title)
@@ -25,7 +25,9 @@ comment_queue = submission.comments[:] # type: ignore
 
 while comment_queue:
     comment = comment_queue.pop(0)
-    comment_dict = {
+    # print(f"comment: {comment.body}")
+    # extract current comment in queue's info as a dict
+    comment_dict = {    
         "comment_id": comment.id, 
         "author": str(comment.author), 
         "body": comment.body, 
@@ -33,11 +35,26 @@ while comment_queue:
         "replies": []
         }
     
-    comment_map[comment.id] = comment_dict
+    # places current comment into comment_map dict
+    # by having comment_id be the key and whole comment_dict as the value
+    comment_map[comment.id] = comment_dict 
 
-    if comment_dict and comment.parent_id.startswith("t1_"):
-        parent_id = comment.parend_id[3:]
-        top_level_comments.append(comment)
+    # if comment exists and is a reply
+    # because replies have parent id prefixed with "t1_"
+    if comment_dict and comment.parent_id.startswith("t3_"): 
+        post_data["comments"].append(comment_dict)
+    else:
 
-for comment in top_level_comments:
-    print(comment.body)
+        # assigns parent id variable which
+        # is the parent submission's id
+        # also strips "t1_" prefix
+        parent_id = comment.parent_id[3:]
+        # print(f"parent id: {parent_id}")
+
+        if parent_id in comment_map: 
+            # if parent id is submission id
+            # append it as a reply in comment map
+            comment_map[parent_id]["replies"].append(comment_dict)
+    comment_queue.extend(comment.replies)
+            
+print(f"Post data: {post_data}")
