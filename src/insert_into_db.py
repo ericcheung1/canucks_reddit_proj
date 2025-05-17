11fdf265f78231637d359dc1ebbd2ae34926e437
ericcheung1 <ericcheun251@gmail.com>
2025-05-17 15:44:17 -0700

    modified files

--- src/insert_into_db.py
+++ src/insert_into_db.py
@@ -3,17 +3,29 @@ from get_comments import fetch_comments
 from authenticate import authenticate_reddit, connect_mongodb
 
 reddit_instance = authenticate_reddit("config.ini")
-comment_forest = fetch_comments(reddit_instance, "1g677aq")
+comment_forest = fetch_comments(reddit_instance, "1gg714l")
 
 client = connect_mongodb("config.ini")
 
-db = client['test_db']
-post_collection = db['posts']
+def insert_posts(client, database, collection, post_data):
+    post_id = post_data["post_id"]
+    db = client[database]
+    collection = db[collection]
+    exists = collection.find_one( { "post_id" : f"{post_id}" } )
 
-try:
-    inserted_result = post_collection.insert_one(comment_forest)
-    print(f"Successfully inserted post with ID: {inserted_result.inserted_id}")
-except Exception as e:
-    print(f"Error in inserting: {e}")
+
+    if exists == None:
+        try:
+            inserted_result = collection.insert_one(post_data)
+            print(f"Successfully inserted post with ID: {inserted_result.inserted_id}")
+            return inserted_result
+        except Exception as e:
+            print(f"Error in inserting: {e}")
+            return None
+    else:
+        print("Post is Already in Collection.")
+        return None
+
+insert_posts(client, "test_db", "posts", comment_forest)
 
 client.close()
