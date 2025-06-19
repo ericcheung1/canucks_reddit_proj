0e7a802df1566426ab971afa50f2460c2f314a21
ericcheung1 <ericcheun251@gmail.com>
2025-06-18 22:15:25 -0700

    modified files

--- web_app/routes.py
+++ web_app/routes.py
@@ -15,8 +15,9 @@ def list_posts():
 @bp.route('/posts/<string:post_id>')
 def post_detail(post_id):
     post = Post.objects(post_id=post_id).first() # type: ignore
+    post_title = post.title.replace("Post Game Thread: ", "")
 
     if post:
-        return render_template("post_detail.html", post=post)
+        return render_template("post_detail.html", post=post, clean_title=post_title)
     else:
         return "Post Not Foun!", 404
