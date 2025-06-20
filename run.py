6de5226f40274ce7635e9bb50deea569051aedc4
ericcheung1 <ericcheun251@gmail.com>
2025-06-19 19:37:12 -0700

    modified file

--- run.py
+++ run.py
@@ -1,6 +1,18 @@
 from web_app import create_app
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 app = create_app()
 
+DEBUG_ENV_VAR = os.getenv("FLASK_DEBUG")
+print(DEBUG_ENV_VAR)
+if DEBUG_ENV_VAR and DEBUG_ENV_VAR.lower() in ("true", "1", "t", "y", "yes"):
+    app.config["DEBUG"] = True
+    print("Flask DEBUG is ON.")
+else:
+    app.config["DEBUG"] = False
+    print("Flask DEBUG is OFF.")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=app.config["DEBUG"])
