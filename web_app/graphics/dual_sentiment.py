5c8da92e4900cc16a933d66605fe49080a82983f
ericcheung1 <ericcheun251@gmail.com>
2025-07-07 23:24:45 -0700

    wip: added new visual route

---
+++ web_app/graphics/dual_sentiment.py
@@ -0,0 +1,31 @@
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import plotly.io as pio
+import pandas as pd
+import datetime as dt
+
+def sentiment_distribution(all_post_obj):
+    VADER_POS = [post.pos_count for post in  all_post_obj]
+    VADER_NEU = [post.neu_count for post in all_post_obj]
+    VADER_NEG = [post.neg_count for post in all_post_obj]
+    Distil_POS = [post.distilbert_pos_count for post in all_post_obj]
+    Distil_NEG = [post.distilbert_neg_count for post in all_post_obj]
+    UTC_CREATED = [post.utc_created for post in all_post_obj]
+
+    df = pd.DataFrame({"D_POS": Distil_POS, "D_NEG": Distil_NEG, "UTC": UTC_CREATED,
+                       "V_POS": VADER_POS, "V_NEU": VADER_NEU, "V_NEG": VADER_NEG})
+    df["UTC"] = pd.to_datetime(df["UTC"], unit="s",utc=True)
+    df["Local_time"] = df["UTC"].dt.tz_convert("America/Los_Angeles")
+
+    fig = make_subplots(rows=1, cols=2, shared_xaxes=True, shared_yaxes=True, subplot_titles=("VADER", "DistilBERT"))
+
+    fig.add_trace(go.Scatter(x=df["Local_time"], y=df["V_POS"], name="VADER Positive", mode="lines"), row=1, col=1)
+    fig.add_trace(go.Scatter(x=df["Local_time"], y=df["V_NEU"], name="VADER Neutral", mode="lines"), row=1, col=1)
+    fig.add_trace(go.Scatter(x=df["Local_time"], y=df["V_NEG"], name="VADER Negative", mode="lines"), row=1, col=1)
+
+    fig.add_trace(go.Scatter(x=df["Local_time"], y=df["D_POS"], name="DistilBERT Positive", mode="lines"), row=1, col=2)
+    fig.add_trace(go.Scatter(x=df["Local_time"], y=df["D_NEG"], name="DistilBERT Negative", mode="lines"), row=1, col=2)
+
+    fig.update_layout(height=675)
+
+    return pio.to_html(fig, full_html=False)
--- web_app/routes.py
+++ web_app/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, url_for
 from .models import Post, Comment, Reply
+from .graphics.dual_sentiment import sentiment_distribution
 from .graphics.percent_positive import create_pct_pos_graph
 
 bp = Blueprint("main", __name__)
@@ -12,9 +13,12 @@ def index():
 def list_posts():
     all_posts = Post.objects().only( # type: ignore
         'title', 'post_id', "total_bodies", 
-        "pos_count", "distilbert_pos_count", "utc_created") 
-    chart_html = create_pct_pos_graph(all_posts)
-    return render_template("list_posts.html", posts=all_posts, chart=chart_html)
+        "pos_count", "neu_count", "neg_count","utc_created",
+        "distilbert_pos_count", "distilbert_neg_count") 
+    chart1 = sentiment_distribution(all_posts)
+    chart2 = create_pct_pos_graph(all_posts)
+    return render_template("list_posts.html", posts=all_posts, 
+                           chart_one=chart1, chart_two = chart2)
 
 @bp.route('/posts/<string:post_id>')
 def post_detail(post_id):
@@ -25,3 +29,9 @@ def post_detail(post_id):
         return render_template("post_detail.html", post=post, clean_title=post_title)
     else:
         return "Post Not Found!", 404
+    
+@bp.route('/visualiztions')
+def draw_graphs():
+    all_posts = Post.objects() 
+
+    return "<h2>In Progress!</h2>"
